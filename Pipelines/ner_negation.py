8af92a8f0e9869d18a4e8f3c0c75049cb8b29dad
wlane <will@asklane.com>
2017-07-06 10:00:14 -0700

    fixed ner_neg pipeline to intake models

--- Pipelines/ner_negation.py
+++ Pipelines/ner_negation.py
@@ -10,12 +10,12 @@ from DataLoading.JSONDataLoader import JSONDataLoader
 from NERExtraction.Extraction import NERExtraction
 
 
-def main(documents, model_type, spacy_model):
+def main(documents, model_type, models):
     text_dl = JSONDataLoader(documents=documents)
-    docs = text_dl.preprocess(spacy_model=spacy_model)
+    docs = text_dl.preprocess(spacy_model=models['spacy'])
 
     extractor = NERExtraction(docs, model_algo=model_type)
-    tagged_documents = extractor.tag_all()
+    tagged_documents = extractor.tag_all(models)
     tagged_documents = extractor.remove_negated_concepts(tagged_documents)
     json_response = extractor.docs2json(tagged_documents)
     print "Done."
